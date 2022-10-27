b7b214ad392833d3a53d7596e9177053ad1633f9
Ashraful610 <ashalam610@gmail.com>
2022-10-28 00:46:09 +0600

    banner is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Component/Home/Home';
 import Navbar from './Component/Shared/Navbar/Navbar';
 
 function App() {
   return (
     <div>
        <Navbar></Navbar>
+       <Home></Home>
     </div>
   );
 }
---
+++ src/Component/Banner/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div className='banner'>
+            <div className='banner-text'>
+              <h2>Computer Engineering</h2>
+              <h3>142,765 Computer Engineers follow this</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
